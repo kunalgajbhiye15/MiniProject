287f75714e1214ab98ea006d4e9c61796d91f1d0
Kunal <gajbhiye85@gmail.com>
2023-08-21 16:13:06 +0530

    Add project files.

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelBinding.Models;
+
+namespace ModelBinding.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: EmployeesController
+        public ActionResult Index()
+        {
+            //List<Employee> lstEmp = new List<Employee>();
+            //lstEmp.Add(new Employee { EmpNo = 1, Name = "Kajal", Basic = 1000000, DeptNo = 30 });
+            //lstEmp.Add(new Employee { EmpNo = 2, Name = "Kalyani", Basic = 1000, DeptNo = 10 });
+            //lstEmp.Add(new Employee { EmpNo = 3, Name = "Kamlesh", Basic = 10000, DeptNo = 30 });
+            //return View(lstEmp);
+
+            List<Employee> lstEmp = Employee.GetAllEmployee();
+            return View(lstEmp);
+
+        }
+
+        public ActionResult Show()
+        {
+            List<Employee> lstEmp = Employee.GetAllEmployee();
+            return View(lstEmp);
+        }
+        
+
+
+
+        // GET: EmployeesController/Details/5
+        public ActionResult Details(int id)
+        {
+            //Employee obj = new Employee();
+            //obj.EmpNo = id;
+            //obj.Name = "Kunal";
+            //obj.Basic = 100000;
+            //obj.DeptNo = 10;
+            //return View(obj);
+
+            Employee obj = Employee.GetSingleEmployee(id);
+            return View(obj);
+        }
+
+        // GET: EmployeesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //public ActionResult Create(IFormCollection collection)
+        //public ActionResult Create(Employee obj)
+        public ActionResult Create(Employee obj,IFormCollection collection,string Name)
+        {
+            try
+            {
+
+                //string EmpNo = collection["EmpNo"];
+                //string Name1 = collection["Name"];
+                //string Basic = collection["Basic"];
+                //string DeptNo = collection["DeptNo"];
+
+                Employee.InsertEmployee(obj);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(obj);
+            }
+        }
+
+        // GET: EmployeesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            //Employee obj = new Employee();
+            //obj.EmpNo = id;
+            //obj.Name = "Kunal";
+            //obj.Basic = 100000;
+            //obj.DeptNo = 10;
+            //return View(obj);
+
+            Employee obj = Employee.GetSingleEmployee(id);
+            return View(obj);
+        }
+
+        // POST: EmployeesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id,Employee obj, IFormCollection collection)
+        {
+            try
+            {
+                Employee.UpdateEmployee(obj);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            //Employee obj = new Employee();
+            //obj.EmpNo = id;
+            //obj.Name = "Kunal";
+            //obj.Basic = 100000;
+            //obj.DeptNo = 10;
+            //return View(obj);
+
+            Employee obj = Employee.GetSingleEmployee(id);
+            return View(obj);
+        }
+
+        // POST: EmployeesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id,Employee obj, IFormCollection collection)
+        {
+            try
+            {
+                Employee.Delete(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,213 @@
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace ModelBinding.Models
+{
+    public class Employee
+    {
+        public int EmpNo { get; set; }
+        public string Name { get; set; }
+        public decimal Basic { get; set; }
+        public int DeptNo { get; set; }
+
+        static string Db = @"server=localhost;userid=root;password=Kunal@1996;database=Dotnet";
+        public static List<Employee> GetAllEmployee()
+        {
+            List<Employee> lstEmp = new List<Employee>();
+
+            MySqlConnection cn = new MySqlConnection();
+            cn.ConnectionString = Db;
+            cn.Open();
+
+            try
+            {
+                //SqlCommand cmd = cn.CreateCommand();
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select * from Employees ";
+
+                MySqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+
+                    Employee obj = new Employee();
+
+                    obj.EmpNo = dr.GetInt32("EmpNo");
+                    obj.Name = dr.GetString("Name");
+                    obj.Basic = dr.GetDecimal("Basic");
+                    obj.DeptNo = dr.GetInt32("DeptNo");
+
+                    lstEmp.Add(obj);
+
+                }
+
+                dr.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+            return lstEmp;
+        }
+
+        public static Employee GetSingleEmployee(int EmpNo)
+        {
+            Employee employee = new Employee();
+            MySqlConnection cn = new MySqlConnection();
+            cn.ConnectionString = Db;
+            cn.Open();
+
+            try
+            {
+                //SqlCommand cmd = cn.CreateCommand();
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select * from Employees where EmpNo=@EmpNo";
+
+                cmd.Parameters.AddWithValue("@EmpNo", EmpNo);
+
+                MySqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    employee.EmpNo = EmpNo;
+                    employee.Name = dr.GetString("Name");
+                    employee.Basic = dr.GetDecimal("Basic");
+                    employee.DeptNo = dr.GetInt32("DeptNo");
+
+                }
+                else
+                {
+                    //if not found
+                    employee = null;
+                }
+                dr.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+            return employee;
+        }
+
+        public static void InsertEmployee(Employee obj)
+        {
+            MySqlConnection cn = new MySqlConnection();
+            cn.ConnectionString = Db;
+            cn.Open();
+
+            try
+            {
+                //SqlCommand cmd = cn.CreateCommand();
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "Insert into Employees values(@EmpNo,@Name,@Basic,@DeptNo)";
+
+                cmd.Parameters.AddWithValue("@EmpNo", obj.EmpNo);
+                cmd.Parameters.AddWithValue("@Name", obj.Name);
+                cmd.Parameters.AddWithValue("@Basic", obj.Basic);
+                cmd.Parameters.AddWithValue("@DeptNo", obj.DeptNo);
+
+
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine("okay");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+       public static void UpdateEmployee(Employee obj)
+        {
+            MySqlConnection cn = new MySqlConnection();
+            cn.ConnectionString = Db;
+            cn.Open();
+
+            try
+            {
+                //SqlCommand cmd = cn.CreateCommand();
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                //cmd.CommandText = "Insert into Employees values(@EmpNo,@Name,@Basic,@DeptNo)";
+                cmd.CommandText = "UPDATE Employees SET Name = @Name, Basic = @Basic, DeptNo = @DeptNo WHERE EmpNo = @EmpNo";
+                cmd.Parameters.AddWithValue("@EmpNo", obj.EmpNo);
+                cmd.Parameters.AddWithValue("@Name", obj.Name);
+                cmd.Parameters.AddWithValue("@Basic", obj.Basic);
+                cmd.Parameters.AddWithValue("@DeptNo", obj.DeptNo);
+
+
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine("okay");
+                Console.WriteLine("Update successful");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+       public static void Delete(int empNo)
+        {
+            MySqlConnection cn = new MySqlConnection();
+            cn.ConnectionString = Db;
+            cn.Open();
+
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "DELETE FROM Employees WHERE EmpNo = @EmpNo";
+
+                cmd.Parameters.AddWithValue("@EmpNo", empNo);
+
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine("Delete successful");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+
+
+    }
+}
